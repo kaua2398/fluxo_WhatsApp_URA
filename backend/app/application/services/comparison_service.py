bfa795d85dc5f00dda88823bbee18e413bbe3acb
Kauã Belchior <kauabelchior50@gmail.com>
2026-07-29 13:53:36 -0300

    feat: adiciona servicos pendentes e arquivo gitignore

---
+++ backend/app/application/services/comparison_service.py
@@ -0,0 +1,8 @@
+class ComparisonService:
+    """Service responsável pela comparação de fluxos/versões."""
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    async def compare_flows(self, *args, **kwargs):
+        return {}
---
+++ backend/app/application/services/upload_service.py
@@ -0,0 +1,8 @@
+class UploadService:
+    """Service responsável pelo upload e processamento de arquivos."""
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    async def process_file(self, *args, **kwargs):
+        return {}
